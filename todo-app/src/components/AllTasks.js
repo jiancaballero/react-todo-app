e6771fe62125a98228c1f465d5202cde11f256ac
jiancaballero <jiancaballero@gmail.com>
2022-08-18 22:44:02 +0900

    delete all merged files

--- todo-app/src/components/AllTasks.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import NoTasks from "./NoTasks";
-
-const AllAtasks = ({ allTasks }) => {
-  return <div className="TaskContainer">{allTasks.length >0 ? allTasks : <NoTasks/>}</div>;
-};
-
-export default AllAtasks;
--- todo-app/src/components/PageNotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PageNotFound = () => {
-  return (
-    <div><h1>PageNotFound</h1></div>
-  )
-}
-
-export default PageNotFound
